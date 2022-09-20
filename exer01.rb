b5133015d5655bc7e89e39043943cb2dab8337d5
eze1er <papaezechiel@gmail.com>
2022-09-20 03:12:32 +0000

    aritmetic op

--- exer01.rb
+++ exer01.rb
@@ -1,3 +1,21 @@
 puts "Hello, what's your name?"
 name = gets.chomp
-puts "Welcome to \"Ruby land\", #{name}. Hope you enjoy your stay (and never leave!)"
+puts "Welcome to \"Ruby land\""
+
+puts 1 + 1 
+puts 8 - 1
+puts 10 * 2
+puts 35 / 5
+puts 2 ** 5 
+puts 5 % 3 
+
+puts 3 & 5
+puts 3 | 5
+puts 3 ^ 5
+
+# Arithmetic is just syntactic sugar
+# for calling a method on an object
+puts 1.+(3)
+puts 10.* 5
+puts "Ruby land"
+
